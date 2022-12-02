42b3e27d492a152c795ed7726c1d8c3ad0309c28
Jean-Baptiste Matte <jean-baptiste.matte@contentsquare-ext.com>
2022-12-02 18:27:13 +0100

    Send screen names when user visit a page

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,6 +1,9 @@
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
+/* Contentsquare plugin */
+import { ContentsquarePlugin } from '@contentsquare/capacitor-plugin';
 
 /* Page components */
 import DynamicVariables from './pages/dynamic-variables/DynamicVariables';
@@ -32,35 +35,69 @@ import '@ionic/react/css/text-transformation.css';
 /* Theme variables */
 import './theme/variables.css';
 import GdprModal from './components/gdpr-modal/GdprModal';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 setupIonicReact();
+
+// We map the url of the screen with the screen name
+const mapScreenURL: Record<string, string> = {
+  "/home": "Menu",
+  "/privacy": "Privacy",
+  "/screen-views": "Screen Views Menu",
+  "/transactions": "Transactions",
+  "/dynamic-variables": "Dynamic Vars",
+  "/screen-views/default": "Screen Default",
+  "/screen-views/screen-modal": "Screen Modal",
+  "/screen-views/screen-modal/modal": "My Modal Screen",
+  "/screen-views/slide/one": "Screen Slide 1",
+  "/screen-views/slide/two": "Screen Slide 2",
+  "/screen-views/slide/three": "Screen Slide 3",
+  "/screen-views/tabs/one": "Tab 1",
+  "/screen-views/tabs/two": "Tab 2",
+  "/screen-views/tabs/three": "Tab 3",
+};
+
+// Some URLs could be excluded. For example, the "screen-slide" is only a container. We only need to send the name of the slide screens
+const excludedURL = [
+  "/screen-views/screen-slide"
+]
 
 const App: React.FC = () => {
 
   const [isOpen, setIsOpen] = useState(localStorage.getItem('userConsent') == null);
+  const location = useLocation();
+
+  useEffect(() => {
+    // We subscribe to React router events. When the URL changes, we send the corresponding screen name
+    const visitedUrl = location.pathname;
+    if (visitedUrl !== '/' && excludedURL.filter(url => visitedUrl !== url).length === 0) {
+      ContentsquarePlugin.sendScreenName(visitedUrl)
+        .then(_ => console.log('Screen name ' + mapScreenURL[visitedUrl] + ' sent'))
+        .catch(e => console.log(e));
+    }
+  }, [location]);
 
   return (
     <IonApp>
       <GdprModal isOpen={isOpen} onDismissModal={() => setIsOpen(false)} />
-      <IonReactRouter>
-        <IonRouterOutlet>
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/dynamic-variables" component={DynamicVariables} />
-          <Route exact path="/privacy" component={Privacy} />
-          <Route exact path="/screen-views" component={ScreenViews} />
-          <Route exact path="/screen-views/default" component={ScreenDefault} />
-          <Route exact path="/screen-views/modal" component={ScreenModal} />
-          <Route exact path="/screen-views/slide" component={ScreenSlide} />
-          <Route path="/screen-views/tabs" component={ScreenTabs} />
-          <Route exact path="/transactions" component={Transactions} />
-          <Route exact path="/">
-            <Redirect to="/home" />
-          </Route>
-        </IonRouterOutlet>
-      </IonReactRouter>
-    </IonApp>
+      <IonRouterOutlet>
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/dynamic-variables" component={DynamicVariables} />
+        <Route exact path="/privacy" component={Privacy} />
+        <Route exact path="/screen-views" component={ScreenViews} />
+        <Route exact path="/screen-views/default" component={ScreenDefault} />
+        <Route exact path="/screen-views/modal" component={ScreenModal} />
+        <Route exact path="/screen-views/slide" component={ScreenSlide} />
+        <Route path="/screen-views/tabs" component={ScreenTabs} />
+        <Route exact path="/transactions" component={Transactions} />
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+      </IonRouterOutlet>
+    </IonApp >
   )
 };
 
 export default App;
+
+
