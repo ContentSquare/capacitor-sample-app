8687f9390e7de1e90301bc39363f0a57c384b820
Jean-Baptiste Matte <jean-baptiste.matte@contentsquare-ext.com>
2022-12-05 17:36:30 +0100

    Send screen name for modal screen

--- React/src/pages/screen-views/screen-modal/ScreenModal.tsx
+++ React/src/pages/screen-views/screen-modal/ScreenModal.tsx
@@ -1,12 +1,20 @@
 import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonModal, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { OverlayEventDetail } from '@ionic/react/dist/types/components/react-component-lib/interfaces';
+import { sendScreenName } from '../../../App';
 import { useRef } from 'react';
 
 const ScreenModal = () => {
 
     const modal = useRef<HTMLIonModalElement>(null);
 
-    function onWillDismiss(_ev: CustomEvent<OverlayEventDetail>) { }
+    // When the user close the modal, this screen sends its name again
+    const onWillDismiss = () => {
+        sendScreenName("/screen-views/screen-modal");
+    }
+
+    // When the user opens the modal, we send the screen name of the modal
+    const presentModal = () => {
+        sendScreenName("/screen-views/screen-modal/modal");
+    }
 
     return (
         <IonPage>
@@ -20,10 +28,10 @@ const ScreenModal = () => {
             </IonHeader>
             <IonContent className="ion-padding">
                 <p>Tapping the Open Modal button will present a modal. When the modal opens it sends a screen view, and when it's closed, this screen will also send a screen view.</p>
-                <IonButton id="open-modal">
+                <IonButton id="open-modal" onClick={presentModal}>
                     Open
                 </IonButton>
-                <IonModal ref={modal} trigger="open-modal" onWillDismiss={(ev) => onWillDismiss(ev)}>
+                <IonModal ref={modal} trigger="open-modal" onWillDismiss={onWillDismiss}>
                     <IonContent className="ion-padding">
                         <p>This is the modal screen. When it's closed, the opener screen will also send its screen name.</p>
                         <IonButton onClick={() => modal.current?.dismiss()}>Close</IonButton>
