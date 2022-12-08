c0e4f317bd9380845cac84021dc93587185c7dca
Jean-Baptiste Matte <jean-baptiste.matte@contentsquare-ext.com>
2022-12-08 17:15:24 +0100

    Enable In app features

--- React/ios/App/App/AppDelegate.swift
+++ React/ios/App/App/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Capacitor
+import ContentsquareModule
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -36,6 +37,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
         // Called when the app was launched with a url. Feel free to add additional processing here,
         // but if you want the App API to support tracking app url opens, make sure to keep this call
+        Contentsquare.handle(url: url)
         return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
     }
 
