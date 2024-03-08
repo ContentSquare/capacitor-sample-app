8c1d4fe8a71dd119473fe3a49b04260dc0c0ccff
jiboune <>
2024-03-08 12:30:01 +0100

    Rebuild ios app with plugin 4.0.0

--- React/ios/App/App/AppDelegate.swift
+++ React/ios/App/App/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Capacitor
-import ContentsquareModule
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -37,7 +36,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
         // Called when the app was launched with a url. Feel free to add additional processing here,
         // but if you want the App API to support tracking app url opens, make sure to keep this call
-        Contentsquare.handle(url: url)
         return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
     }
 
